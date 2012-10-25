cb0837eb363d1ca337f188804e6d45510210962c
Dan F-M <danfm@nyu.edu>
2012-10-24 20:53:52 -0400

    computing lightcurves

--- transit/transit.py
+++ transit/transit.py
@@ -4,104 +4,141 @@
 
 """
 
-from __future__ import division
 
 __all__ = ['Transit']
 
 import numpy as np
+import scipy.optimize as op
 
-class Transit(object):
-    def __init__(self, R, Rs, Rp, incl):
-        self.R  = R
-        self.Rs = Rs
-        self.p  = Rp/Rs
+# Planet parameter priors.
+
+Rmin = 0.1    # M_earth
+Rmax = 50.0   # M_earth
+nR = -1.5
+
+
+def occultation(p, z):
+    z = np.atleast_1d(np.abs(z))
+
+    m1 = z >= 1 + p
+    m2 = (np.abs(1 - p) < z) * (z < 1 + p)
+    m3 = z <= 1 - p
+    m4 = z <= p - 1
+
+    p2 = p ** 2
+    z2 = z[m2] ** 2
+    tmp = z2 - p2 + 1
+    k1 = np.arccos(0.5 * tmp / z[m2])
+    k2 = np.arccos(0.5 * (z2 + p2 - 1) / z[m2] / p)
+    k3 = np.sqrt(z2 - 0.25 * tmp ** 2)
+
+    z[m1] = 0
+    z[m2] = (k1 + p ** 2 * k2 - k3) / np.pi
+    z[m3] = p ** 2
+    z[m4] = 1
+
+    return 1 - z
+
+
+class Planet(object):
+
+    def __init__(self, radius, period, incl, phi, e, a):
+        self.radius = radius
+        self.T = period
         self.incl = incl
-        self.mu = np.cos(incl)
+        self.phi = phi
+        self.e = e
+        self.a = a
 
-    def evaluate(self, phase):
-        phase = np.atleast_1d(phase)
-        m = np.cos(self.incl)*np.cos(phase) > 0
-        z = np.sqrt(np.sin(phase)**2 + np.sin(self.incl)**2*np.cos(phase)**2) \
-                * self.R/self.Rs
-        if len(phase) == 1:
-            if m:
-                return float(self.occultation(z))
-            return 1
-        ret = np.ones_like(phase)
-        ret[m] = self.occultation(z[m])
-        return ret
+    def eccentric_anomaly(self, t):
+        """
+        Numerically solve for the eccentric anomaly as a function of time.
 
-    def occultation(self, z):
-        p = self.p
-        z = np.atleast_1d(np.abs(z))
+        """
+        e = self.e
+        wt = 2 * np.pi * np.atleast_1d(t) / self.T + self.phi
+        psi0s = wt + e * np.sin(wt)
+        f = lambda psi, wt0: wt0 - psi + e * np.sin(psi)
+        return np.array(map(lambda (psi0, wt0): op.newton(f, psi0,
+                                                args=(wt0,)), zip(psi0s, wt)))
 
-        m1 = z >= 1+p
-        m2 = (np.abs(1-p) < z) * (z < 1+p)
-        m3 = z <= 1-p
-        m4 = z <= p-1
+    def coords(self, t, i0=0.0):
+        psi = self.eccentric_anomaly(t)
+        cpsi = np.cos(psi)
 
-        p2 = p**2
-        z2 = z[m2]**2
-        tmp = z2-p2+1
-        k1 = np.arccos(0.5*tmp/z[m2])
-        k2 = np.arccos(0.5*(z2+p2-1)/z[m2]/p)
-        k3 = np.sqrt(z2-0.25*tmp**2)
+        e = self.e
+        d = 1 - e * cpsi
 
-        z[m1] = 0
-        z[m2] = (k1 + p**2 * k2 - k3)/np.pi
-        z[m3] = p**2
-        z[m4] = 1
+        cth = (cpsi - e) / d
+        r = self.a * d
 
-        return 1-z
+        # In the plane of the orbit.
+        x, y = r * cth, np.sign(np.sin(psi)) * r * np.sqrt(1 - cth * cth)
 
-    def animate(self, dphase=0.1):
-        import time
-        import matplotlib.pyplot as pl
-        import matplotlib.patches as mpatches
+        # Rotate into observable coordinates.
+        i = i0 + self.incl
+        return x * np.cos(i), y, x * np.sin(i)
 
-        phase = 0
 
-        pl.ion()
-        pl.figure(figsize=(8,8))
-        ax = pl.axes([0,0.3,1,0.7], aspect='equal', frameon=False,
-                xticks=[], yticks=[])
-        ax2 = pl.axes([0.1,0.1,0.8,0.2])
+class PlanetarySystem(object):
 
-        ph = np.linspace(-np.pi, np.pi, 2000)
-        lc = self.evaluate(ph)
-        ax2.plot(ph/np.pi, lc, 'k')
-        ax2.set_ylim(0.9*np.min(lc), 1.1)
-        pt, = ax2.plot(0, self.evaluate(0), "or")
+    def __init__(self, radius, incl):
+        self.radius = radius
+        self.incl = incl
+        self.planets = []
 
-        star = mpatches.Circle([0, 0], 1, color="w", ec="k")
-        ax.add_patch(star)
+    @classmethod
+    def sample_prior(cls, Np=1):
+        r0 = 150 + 20 * np.random.randn()
+        i0 = 0.0 * np.radians(5 * np.random.randn())
 
-        planet = mpatches.Circle([0, self.R/self.Rs*np.sqrt(1-self.mu**2)],
-                self.p, color="gray", ec="k")
-        ax.add_patch(planet)
-        ax.set_xlim((self.R/self.Rs+self.p)*np.array([-1,1]))
-        ax.set_ylim([-2,2])
+        ps = cls(r0, i0)
 
-        for i in range(100):
-            phase += dphase
-            x = np.sin(phase) * self.R/self.Rs
-            y = -np.sin(self.incl) * np.cos(phase) * self.R/self.Rs
-            planet.center = [x,y]
+        radius = ((Rmax ** (nR + 1) - Rmin ** (nR + 1)) * np.random.rand(Np)
+                + Rmin ** (nR + 1)) ** (1.0 / (1 + nR))
+        period = 0.5 + 500 * np.random.rand(Np)
+        incl = 0.0 * np.radians(0.05 * np.random.randn(Np))
+        phase = 2 * np.pi * np.random.rand(Np)
+        e = 0.3 * np.random.rand(Np)
+        a = 500 + 100 * np.random.randn(Np)
 
-            z = np.cos(self.incl)*np.cos(phase)
-            if z < 0:
-                star2 = mpatches.Circle([0, 0], 1, color="w", ec="k")
-                ax.add_patch(star2)
+        for n in range(Np):
+            ps.add_planet(radius[n], period[n], incl[n], phase[n], e[n], a[n])
 
-            pt.set_xdata((phase/np.pi+1)%2-1)
-            pt.set_ydata(self.evaluate(phase))
+        return ps
 
-            pl.draw()
-            if z < 0:
-                star2.remove()
-            time.sleep(0.05)
+    def add_planet(self, r, T, i, phi, e, a):
+        r0 = self.radius
+        self.planets.append(Planet(r / r0, T, i, phi, e, a / r0))
+
+    def lightcurve(self, t):
+        lc = np.ones_like(t)
+        for p in self.planets:
+            x, y, z = p.coords(t, i0=self.incl)
+            m = x > 0
+            D = np.sqrt(y[m] ** 2 + z[m] ** 2)
+            lc[m] *= occultation(p.radius, D)
+        return lc
+
 
 if __name__ == '__main__':
-    transit = Transit(10, 1, 0.2, np.radians(5))
-    transit.animate()
+    import matplotlib.pyplot as pl
 
+    np.random.seed(42)
+    ps = PlanetarySystem(10.0, -1.3)
+
+    # Hot Jupiter.
+    T = 0.05
+    ps.add_planet(1.2, T, 1.3, np.pi, 0.01, 108)
+
+    # Jupiter.
+    # ps.add_planet(1.0, 12.0, 1.3, 0.0, 0.05, 11000)
+
+    # Saturn.
+    # ps.add_planet(0.85, 30.0, 2.5, 0.0, 0.05, 21000)
+
+    t = np.linspace(0, 15.8, 8491)
+    lc = ps.lightcurve(t)
+
+    pl.plot(t % T, lc, ".k")
+    pl.savefig("lightcurve.png")
